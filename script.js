4d213d326e7936ba1b7027e796399636f5b06a6f
ABDULAZISFAHMI <zaenalarifinfun123@gmail.com>
2025-12-15 12:55:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function login() {
-  window.location.href = "home.html";
-}
-
 function addToCart(name, price) {
   cart.push({name, price});
   localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Barang ditambahkan ke keranjang");
+  alert("Produk ditambahkan ke keranjang");
 }
 
 function loadCart() {
   let items = document.getElementById("cartItems");
   let total = 0;
+
+  items.innerHTML = "";
+
   cart.forEach(item => {
     items.innerHTML += `<p>${item.name} - Rp ${item.price}</p>`;
     total += item.price;
   });
+
   document.getElementById("total").innerText = "Total: Rp " + total;
 }
 
-if(document.getElementById("cartItems")) {
+if (document.getElementById("cartItems")) {
   loadCart();
 }
